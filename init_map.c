714da9f22c2c7c119e137fee8990331e8b9a1491
Soraya <soraya@MacBook-Air-de-Soraya.local>
2026-02-06 21:47:52 +0100

    newline

--- init_map.c
+++ init_map.c
@@ -6,7 +6,7 @@
 /*   By: soraya <soraya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 10:52:03 by sdabbas           #+#    #+#             */
-/*   Updated: 2026/02/06 17:15:12 by soraya           ###   ########.fr       */
+/*   Updated: 2026/02/06 21:44:53 by soraya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,21 @@ static int  count_line(char *argv)
     close(fd_map);
     return (count_line);
 }
+static void removed_newline(t_game *game)
+{
+    int     i;
+    char    len;
 
+    len = 0;
+    i = 0;
+    while (game->map[i])
+    {
+        len = ft_strlen(game->map[i]);
+        if (len > 0 && game->map[i][len - 1] == '\n')
+            game->map[i][len -1] = '\0';
+        i++;
+    }
+}
 void    read_map(char *argv, t_game *game)
 {
     int     nb_line;
@@ -49,6 +63,8 @@ void    read_map(char *argv, t_game *game)
     while (i < nb_line)
     {
        game->map[i] = get_next_line(fd_map);
+       removed_newline(game);
+       printf("%zu\n", ft_strlen(game->map[i]));
        i++;
     }
     game->map[i] = NULL;
